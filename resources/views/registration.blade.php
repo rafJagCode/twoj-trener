350eb6ff685096b2612be756d8c1f765f3f52ed2
Kasia <kosowska0708912@gmail.com>
2020-05-06 21:42:08 +0200

    podpiecie styloów

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,1 +1,14 @@
-<h1> Tutuł</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="{{ asset('/css/registration.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1> Tytuł</h1>
+    
+    </body>
+</html>
