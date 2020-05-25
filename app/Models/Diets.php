b4b8e69a1babfa8e7fa6c19554260bbfcb3ffe1d
szymon9819 <szymon9819@gmail.com>
2020-05-25 20:16:17 +0200

    detale

--- app/Models/Diets.php
+++ app/Models/Diets.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -8,9 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Diets extends Model
 {
     public function user(){
-     
+
         return $this->belongsToMany(User::class, 'diets_has_users', 'users_id', 'diets_id')->withTimestamps();
-     
-     
     }
 }
