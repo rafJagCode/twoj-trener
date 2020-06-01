0dcdbed6675d47116f0eb7570a958c6572852946
rafalski_._ <rafal13piotrkowski@gmail.com>
2020-06-01 09:36:17 +0200

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -33,57 +33,19 @@ class UsersController extends Controller
   public function store(Request $request)
   {
 
-    print_r($request->input(''));
-
-    /*   $user = new User();
-        $user->firstName = $request->input('firstName');
-        $user->secondName = $request->input('secondName'); */
-    /*  $user->city = $request->input('city'); */
-    /*   $user->phoneNumber = $request->input('phoneNumber');
-        $user->save();
-         */
+  
   }
 
   public function show()
   {
-    $id = auth()->user('id');
-    $loggedUser = auth()->user();
-    $disciplines = Dysciplines::all();
-    $diets = $loggedUser->diets()->get();
-
-    $cities = User::pluck('city');
-
-    return view('users.trainer_dashboard', compact('loggedUser', 'disciplines', 'cities', 'diets'));
+  
   }
 
   public function edit(user $user)
   { }
   public function update(Request $request)
   {
-    echo ($request);
-    dd(request('firstName'));
-    dd(request('secondName'));
-    dd(request('city'));
-    dd(request('discipline'));
 
-    $id = auth()->user('id');
-    $user = auth()->user();
-
-    $user->firstName = $request->input('firstName');
-    $user->secondName = $request->input('secondName');
-    $user->city = $request->input('city');
-    $user->update();
-
-    /* nie wiem co robie  */
-    $idDyscyplines;
-    $disciplines = request('discipline');
-
-    foreach ($disciplines as $discipline) {
-      $idDyscyplines = Dysciplines::
-      where("name", $discipline)->get('id');
-
-      DysciplinesHasUsers::where('users_id', $id)
-        ->update();
     };
 
    /*  return redirect('/trainer_dashboard'); */
