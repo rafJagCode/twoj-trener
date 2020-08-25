bc9ad830f5e3ef7c04829b1aef0cf3fc9dc30e51
Łukasz <lukasz.ulenberg@onet.pl>
2020-08-25 19:08:57 +0200

    koniec

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\Message;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Pusher\Pusher;
+
+class MessageController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function index(){
+
+        //$users = User::where('id', '!=', Auth::id())->get();
+        $users = DB::select("select users.id, users.firstName, users.avatar, users.email, count(is_read) as unread 
+        from users LEFT  JOIN  messages ON users.id = messages.from and is_read = 0 and messages.to = " . Auth::id() . "
+        where users.id != " . Auth::id() . " 
+        group by users.id, users.firstName, users.avatar, users.email");
+
+        return view('message', ['users' => $users]);
+    }
+    public function getMessage($user_id)
+    {
+        $my_id = Auth::id();
+
+        Message::where(['from' => $user_id, 'to' => $my_id])->update(['is_read' => 1]);
+
+        $messages = Message::where(function ($query) use ($user_id, $my_id) {
+            $query->where('from', $my_id)->where('to', $user_id);
+        })->orWhere(function ($query) use ($user_id, $my_id) {
+            $query->where('from', $user_id)->where('to', $my_id);
+        })->get();
+        return view('messages.index', ['messages' => $messages]);
+    }
+    public function sendMessage(Request $request)
+    {
+        $from = Auth::id();
+        $to = $request->receiver_id;
+        $message = $request->message;
+
+        $data = new Message();
+        $data->from = $from;
+        $data->to = $to;
+        $data->message = $message;
+        $data->is_read = 0;
+        $data->save();
+
+        $options = array(
+            'cluster' => 'eu',
+            'useTLS' => true
+        );
+
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+
+        $data = ['from' => $from, 'to' => $to];
+        $pusher->trigger('my-channel', 'my-event', $data);
+    }
+}
---
+++ resources/views/message.blade.php
@@ -0,0 +1,248 @@
+@extends('layout')
+@section('head')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ URL::asset('css/fullcalendar.css')}}" />
+<link rel="stylesheet" href="{{ URL::asset('css/jquery.datetimepicker.min.css')}}" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+<style>
+    /* width */
+    ::-webkit-scrollbar {
+        width: 7px;
+    }
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #a7a7a7;
+    }
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #929292;
+    }
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        list-style: none;
+    }
+    .user-wrapper, .message-wrapper {
+        border: 1px solid #dddddd;
+        overflow-y: auto;
+    }
+    .user-wrapper {
+        height: 600px;
+    }
+    .user {
+        cursor: pointer;
+        padding: 5px 0;
+        position: relative;
+    }
+    .user:hover {
+        background: #eeeeee;
+    }
+    .user:last-child {
+        margin-bottom: 0;
+    }
+    .pending {
+        position: absolute;
+        left: 13px;
+        top: 9px;
+        background: #b600ff;
+        margin: 0;
+        border-radius: 50%;
+        width: 18px;
+        height: 18px;
+        line-height: 18px;
+        padding-left: 5px;
+        color: #ffffff;
+        font-size: 12px;
+    }
+    .media-left {
+        margin: 0 10px;
+    }
+    .media-left img {
+        width: 64px;
+        border-radius: 64px;
+    }
+    .media-body p {
+        margin: 6px 0;
+    }
+    .message-wrapper {
+        padding: 10px;
+        height: 536px;
+        background: #eeeeee;
+    }
+    .messages .message {
+        margin-bottom: 15px;
+    }
+    .messages .message:last-child {
+        margin-bottom: 0;
+    }
+    .received, .sent {
+        width: 45%;
+        padding: 3px 10px;
+        border-radius: 10px;
+    }
+    .received {
+        background: #ffffff;
+    }
+    .sent {
+        background: #3bebff;
+        float: right;
+        text-align: right;
+    }
+    .message p {
+        margin: 5px 0;
+    }
+    .date {
+        color: #777777;
+        font-size: 12px;
+    }
+    .active {
+        background: #eeeeee;
+    }
+    input[type=text] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 15px 0 0 0;
+        display: inline-block;
+        border-radius: 4px;
+        box-sizing: border-box;
+        outline: none;
+        border: 1px solid #cccccc;
+    }
+    input[type=text]:focus {
+        border: 1px solid #aaaaaa;
+    }
+</style>
+ 
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
+<script src="{{ URL::asset('js/jquery.datetimepicker.full.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+@endsection
+@section('content')
+ 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">  
+            <div class="user-wrapper">
+                <ul class="users">
+                    @foreach($users as $user)
+                        <li class="user" id="{{$user->id}}">
+                                @if($user->unread)
+                                <span class="pending">{{$user->unread}}</span>
+                                @endif
+                            <div class="media">
+ 
+                                <div class="media-left">
+                                    <img src="{{$user->avatar}}" alt="" class="media-object">
+                                </div>
+ 
+                                <div class="media-body">
+                                    <p class="name">{{$user->firstName}}</p>
+                                    <p class="email">{{$user->email}}</p>
+                                </div>
+ 
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+ 
+        <div class="col-md-8" id="messages">
+           
+ 
+        </div>
+ 
+    </div>
+</div>
+<script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    var receiver_id = '';
+    var my_id = "{{Auth::id()}}";
+    $(document).ready(function(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('303fe3582f568f111ddf', {
+            cluster: 'eu'
+        });
+
+        var channel = pusher.subscribe('my-channel');
+        channel.bind('my-event', function(data) {
+            alert(JSON.stringify(data));
+            if (my_id == data.to) {
+                $('#' + data.from).click();
+            } else if (my_id == data.to) {
+                if(receiver_id == data.from) {
+                    $('#' + data.from).click();
+                } else {
+                    $('#' + data.from).append('<span class="pending">1</span>');
+                }
+            }
+        });
+
+
+        $('.user').click(function(){
+            $('.user').removeClass('active');
+            $(this).addClass('active');
+            $(this).find('.pending').remove();
+
+            receiver_id = $(this).attr('id');
+            $.ajax({
+                type: "get",
+                url: "message/" + receiver_id,
+                data: "",
+                cache: false,
+                success: function (data){
+                    $('#messages').html(data);
+                    scrollToBottom();
+                }
+            });
+        });
+        
+        $(document).on('keyup', '.input-text input', function (e) {
+            var message = $(this).val();
+            if (e.keyCode == 13 && message != '' && receiver_id != '') {
+                $(this).val(''); // while pressed enter text box will be empty
+                var datastr = "receiver_id=" + receiver_id + "&message=" + message;
+                $.ajax({
+                    type: "post",
+                    url: "message", // need to create this post route
+                    data: datastr,
+                    cache: false,
+                    success: function (data) {
+                    },
+                    error: function (jqXHR, status, err) {
+                    },
+                    complete: function () {
+                        scrollToBottom();
+                    }
+                })
+            }
+        });
+
+        function scrollToBottom(){
+            $('.message-wrapper').animate({
+                scrollTop: $('.message-wrapper').get(0).scrollHeight
+            }, 50);
+        }
+    });
+</script>
+@endsection
