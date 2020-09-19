8fec34b81d6893176154ab9e32e722163d113699
szym3ns <horbalszymon@gmail.com>
2020-09-19 18:53:42 +0200

    User dashboard and user_show got new look

--- resources/views/users/user_dashboard.blade.php
+++ resources/views/users/user_dashboard.blade.php
@@ -1,60 +1,62 @@
-@extends('app')
+@extends('layout')
 
-@section('title', 'trainer_dashboard')
+@section('title', 'user_dashboard')
 
-@section('trainer-dashboard-css')
-    <link href="{{ asset('/css/trainer_dashboard.css') }}" rel="stylesheet">
+@section('additional-css')
+    <link href="{{ asset('/css/slick.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/additional.css') }}" rel="stylesheet">
 @endsection
 
-@section('trainer-dashboard')
-    <div id="wrapper">
-        <div class="container">
 
-            <div class="row text-center">
-                <div class="col-lg-12 ">
-                    <div class="alert alert-warning">
-                        <strong>Witaj {{$user->firstName}} {{$user->secondName}} ! </strong> Twoje nowe wiadomości: {{$user->unread}}
+@section('content')
+    <section class="featured popular portfolio freelancers rec-pro-1">
+        <div class="container-fluid">
+            <div id="wrapper">
+
+                <div class="row text-center">
+                    <div class="col-lg-12 ">
+                        <div class="alert alert-warning">
+                            <strong>Witaj {{$user->firstName}} {{$user->secondName}}! </strong> Twoje nowe wiadomości: {{$user->unread}}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row text-center">
 
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <a href="#" class="no-underline">
-                        <div class="div-square py-3">
-                            <i class="fas fa-search fa-5x"></i>
-                            <h4>Znajdź trenera</h4>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <a href="{{ route('user.index', $user->id) }}" class="no-underline">
-                        <div class="div-square py-3">
-                            <i class="fas fa-pen fa-5x"></i>
-                            <h4>Twoje dane</h4>
-                        </div>
-                    </a>
-                </div>
+                <div class="row text-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <a href="#" class="no-underline">
+                            <div class="div-square py-3">
+                                <i class="fas fa-search fa-5x"></i>
+                                <h4>Znajdź trenera</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <a href="{{ route('user.index', $user->id) }}" class="no-underline">
+                            <div class="div-square py-3">
+                                <i class="fas fa-pen fa-5x"></i>
+                                <h4>Twoje dane</h4>
+                            </div>
+                        </a>
+                    </div>
 
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <a href="{{ route('fullcalendar.index' , $user->id)}}" class="no-underline">
-                        <div class="div-square py-3">
-                            <i class="far fa-calendar-times fa-5x"></i>
-                            <h4>Kalendarz</h4>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <a href="{{'message'}}" class="no-underline">
-                        <div class="div-square py-3">
-                            <i class="fas fa-comments fa-5x"></i>
-                            <h4>Wiadomości</h4>
-                        </div>
-                    </a>
-                </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <a href="{{ route('fullcalendar.index' , $user->id)}}" class="no-underline">
+                            <div class="div-square py-3">
+                                <i class="far fa-calendar-times fa-5x"></i>
+                                <h4>Kalendarz</h4>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <a href="{{'message'}}" class="no-underline">
+                            <div class="div-square py-3">
+                                <i class="fas fa-comments fa-5x"></i>
+                                <h4>Wiadomości</h4>
+                            </div>
+                        </a>
+                    </div>
             </div>
         </div>
-    </div>
-
-
+    </section>
 @endsection
