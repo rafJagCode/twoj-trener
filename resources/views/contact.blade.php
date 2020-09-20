cf10fff112a75bdae1d0f407aab0a0e0624b452b
szym3ns <horbalszymon@gmail.com>
2020-09-20 21:23:27 +0200

    Location now works fine

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,16 +10,19 @@
     <script src="{{ asset('/js/leaflet.js') }}"></script>
 
     <script type="text/javascript">
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    {
+        attribution: false
+    });
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'your.mapbox.access.token'
-        }).addTo(mymap);
+        var mymap = L.map('mapid',
+            {
+                zoomControl: true,
+                layers: [tileLayer],
+                maxZoom: 18,
+                minZoom: 6
+            })
+            .setView([53.763914, 20.520403], 15);
     </script>
 @endsection
 
